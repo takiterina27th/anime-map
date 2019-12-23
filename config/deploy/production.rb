b50e8faf31876acb33ae22a248258cc9065fa7c1
takiterina27th <nozomeel5296@yahoo.co.jp>
2019-12-23 23:05:08 +0900

    production.rbを編集
    詳細：
    production.rbにデプロイする環境の設定を記述。

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '54.95.185.15', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
