df4a34b76332a31a257fd89a37d0834c8c5764d5
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-09 14:26:59 +0900

    サインアウト後のリダイレクト先URLを指定した
    詳細：
    after_sign_out_path_forは返り値にサインアウト後のリダイレクト先URLを指定できる。
    deviseのメソッドを上書きしている関係上resourceを引数に渡さなけらばならないので、resourceを引数に渡した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
 end
