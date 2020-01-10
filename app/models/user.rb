3403e07ae966d32e9b260f2ce0491e7d1ed7a0f2
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-10 20:12:01 +0900

    emailのバリデーションを削除
    目的：
    emailのバリデーションはdeviseによりデフォルトで設定されているため、別に記述すると、エラーメッセージが重複して表示されてしまう。
    それを避けるためにモデル内のemailのバリデーションを削除した。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,          presence: true, length: { maximum: 20}
-  validates :email,             presence: true, uniqueness: true
 end
