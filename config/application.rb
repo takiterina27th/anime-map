a0fcdd7aeffbb0abcd8c9597a54224721ca91ca0
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-10 21:29:41 +0900

    application.rbにこのymlファイルの読み込む一文を追記した
    詳細：
    正規表現を使い、config/locales以下のディレクトリ内にある全てのymlファイルを読み込むようにした。

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module AnimeMap
     # the framework and any gems in your application.
 
     config.i18n.default_locale = :ja
-    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
   end
 end
