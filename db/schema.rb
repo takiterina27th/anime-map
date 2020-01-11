05fcb670078a9a6b353a4fb88e1c56a9a16ba6e5
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-11 22:22:19 +0900

    rails db:migrate
    詳細：
    rails db:migrateを行いスキーマファイルに反映させた。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_053459) do
+ActiveRecord::Schema.define(version: 2020_01_11_130855) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "image", null: false
+    t.string "anime_title", null: false
+    t.string "place", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_posts_on_name"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema.define(version: 2020_01_08_053459) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
