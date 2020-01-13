e2c8240f496c8a360f093efbdb181ab23ed5eb1f
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-14 01:19:01 +0900

    indexアクションに@postsインスタンス変数を追加
    目的：
    商品を情報を取得し一覧で表示するため。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :display, :search, :show]
 
   def index
+    @posts = Post.all
   end
 
   def display
