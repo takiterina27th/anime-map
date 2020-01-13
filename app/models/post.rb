2aeb714f465bb63dee8147348290dea4ea028cde
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-13 19:58:48 +0900

    バリデーションの記述を変更した
    詳細：
    :messageオプションを利用することで、メッセージを変更した。

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :image, presence: true
+  validates :image, presence: { message: 'を選択してください。' }
   validates :place, presence: true
   validates :anime_title, presence: true
   validates :description, presence: true
