2e83ba7d7a731ba24c28ee03767782a31bc44f9d
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-13 19:14:54 +0900

    投稿にバリデーションを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
 
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :place, presence: true
+  validates :anime_title, presence: true
+  validates :description, presence: true
+
   mount_uploader :image, ImageUploader
 end
