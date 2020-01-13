7cf0dce146023b8b6f3f20871f5440eb99e1e70b
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-14 01:24:07 +0900

    displayアクションに@postsインスタンス変数を記述し直した。
    目的：
    投稿一覧表示ページなので、displayアクションに記述する必要があるため。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,10 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :display, :search, :show]
 
   def index
-    @posts = Post.all
   end
 
   def display
+    @posts = Post.all
   end
 
   def search
