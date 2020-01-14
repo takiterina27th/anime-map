562730932bcabd82800f7a5a2cde437240386385
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-14 21:50:17 +0900

    application_controller.rbにset_searchアクションを作成
    詳細：
    検索フォームはuserページやpostページのヘッダーに設置予定のため、application_controllerにアクションを記述した。
    posts_controllerに書くとposts_controllerを使わないページ(users_controllerを使うuser関連のページ)を表示した時にエラーになるため。
    
    また、searchメソッドよりransackメソッド推奨なのでransackメソッドを使った。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def set_search
+    @q = Post.ransack(params[:q])
+    @search_posts = @q.result(distinct: true)
+  end
 
   protected
 
