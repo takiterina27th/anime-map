ae021df1b2dfadc04c3b230654d091902c72ba27
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-14 19:16:33 +0900

    updateアクションを追加
    詳細：
    paramsでpostテーブルの情報を取得し、updateメソッドで上書き処理。
    リダイレクト先を投稿詳細ページに指定した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,12 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
   def destroy
     @post = Post.find(params[:id])
     if @post.user_id == current_user.id
