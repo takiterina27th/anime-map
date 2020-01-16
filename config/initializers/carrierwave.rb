22d3ca8bdf055e66c1a67906d788085e4e2bb6e9
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-16 18:41:14 +0900

    Rails.env.production?で条件分岐
    詳細：
    本番環境とそうでない場合でcarrierwaveの設定を場合分けした。

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,19 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'takiterina-bucket'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takiterina-bucket'
+  if Rails.env.production? || Rails.env.staging?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'takiterina-bucket'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/takiterina-bucket'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
