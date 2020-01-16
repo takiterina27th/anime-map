dec29b13db9d171a6fc90227aa2913fb24d284bf
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-16 21:41:58 +0900

    コントローラーに.page(params[:page]).per(4)を追加し、= paginate @postsで適用した
    詳細：
    pageメソッド、perメソッドを設定した。
    paginateと繰り返す変数は同じものを設定する。
    @postsをcontrollerの設定で繰り返し、@postsでpaginateも設定した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   end
 
   def display
-    @posts = Post.order('id DESC').limit(24).includes(:user)
+    @posts = Post.order('id DESC').page(params[:page]).per(4).includes(:user)
   end
 
   def search
