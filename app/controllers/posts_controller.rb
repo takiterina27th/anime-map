233aca09be1ccef3d5fa83fa6d9471d7f74466d2
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-17 23:44:51 +0900

    postコントローラーのshowアクションに@comments及び@commentを追加
    詳細：
    @comments = @post.comments
    は投稿詳細ページでコメントを一覧で表示するため、
    @comment = @post.comments.build
    は投稿詳細ページでコメントフォームを作成するためにそれぞれ設定した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,8 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comments = @post.comments
+    @comment = @post.comments.build
   end
 
   def mypage
