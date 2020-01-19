eca69a4c76a6928e01d0a2ef5bb3c0ba1fdc1e48
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-19 12:33:03 +0900

    user.rbにアソシエーションを設定
    詳細：
    userテーブルとlikeテーブルは１対多の関係なのでhas_many :likes。
    ユーザーが削除されたらいいねも削除する必要があるためdependent: :destroyを設定。
    liked_postsによってuserがどの投稿をいいねしているのかを取得できるようにした。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   
   has_many :posts
   has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   
   validates :nickname,          presence: true, length: { maximum: 20}
 end
