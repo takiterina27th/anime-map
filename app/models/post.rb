2d225e02d3c1f210c769a4f5aa2fe3a34976ef71
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-19 12:39:37 +0900

    post.rbにアソシエーションを設定
    詳細：
    postテーブルとlikeテーブルは１対多の関係なのでhas_many :likes。
    投稿が削除されたらいいねも削除する必要があるためdependent: :destroyを設定。
    liked_usersによって投稿が誰にいいねされているのかを簡単に取得した。

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   validates :name, presence: true
   validates :image, presence: { message: 'を選択してください。' }
