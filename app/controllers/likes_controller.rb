b506779d601e14db26557a3cb3cb3b4ee63017ca
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-19 13:05:35 +0900

    createアクション及びdestroyアクションを定義
    詳細：
    ・createアクションでいいねをする。
    Likeモデルにuser_idとpost_idを格納する。
    ・destroyアクションでいいねを取り消す
    Likeモデルのレコードを削除する

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,13 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = Like.create(user_id: current_user.id, post_id: @post.id)
+    redirect_back(fallback_location: post_path(@post.id))
+  end
+
+  def destroy
+    @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @like.destroy
+    redirect_back(fallback_location: post_path(params[:post_id]))
+  end
 end
