dbac5ac7c288c661bda17835cff7f3d08da10d9a
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-01-19 19:26:09 +0900

    ランダム表示
    詳細：
    order("RAND()")を用いてトップページの投稿をランダムで表示した。
    mysqlを使用しているのでRANDOMをRANDとした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :display, :search, :show]
 
   def index
-    @posts = Post.order('id DESC').limit(8).includes(:user)
+    @posts = Post.order("RAND()").limit(8).includes(:user)
   end
 
   def display
