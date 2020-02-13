e41a8789b26358b1c79fbe2b400a16512b6e4837
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-13 10:38:18 +0900

    createアクション内でコメントを保存し、respond_toを使用してHTMLとJSONの場合で処理を分ける
    詳細：
    createアクション内でコメントを保存する。newメソッド→saveメソッドをcreateメソッドに変更。
    user_idをmergeメソッド内で取得できるように記述を変更した。
    respond_toを使用してHTMLとJSONの場合で処理を分けた。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,27 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
-    @comment.user_id = current_user.id
-    if @comment.save
-      redirect_back(fallback_location: root_path)
-    else
-      redirect_back(fallback_location: root_path)
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to post_path(params[:post_id])  }
+      format.json
     end
   end
+
+  # def create
+  #   @comment = Comment.new(comment_params)
+  #   @comment.user_id = current_user.id
+  #   if @comment.save
+  #     redirect_back(fallback_location: root_path)
+  #   else
+  #     redirect_back(fallback_location: root_path)
+  #   end
+  # end
 
   private
   def comment_params
     params.require(:comment).permit(
       :text
     )
-    .merge(post_id: params[:post_id])
+    .merge(post_id: params[:post_id], user_id: current_user.id)
   end
 end
