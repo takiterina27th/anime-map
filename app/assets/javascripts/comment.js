dca6b19dcf79a696bce8de6127e9177cf7fea429
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-13 10:49:38 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを作成
    詳細：
    非同期通信の結果として返ってくるデータを、done(function(data) { 処理 })の関数の引数で受け取る。
    $('.form__submit').prop('disabled', false);は、 htmlの仕様でsubmitボタンを一度押したらdisabled属性によりボタンが押せなくなるため、disabled属性をfalseにする記述を追加した。
    
    HTMLを作成するに際して、var text = comments.replace(/\n|\r\n|\r/g, '<br>');を追加してtextに改行を反映させた。

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,4 +1,22 @@
 $(function(){
+  function buildHTML(comment){
+    var comments = comment.text ? `${comment.text}` : "";
+    var text = comments.replace(/\n|\r\n|\r/g, '<br>');
+    var html = `<div class= "comment__body">
+                  <div class= "comment__body__head">
+                    <div class= "comment__body__head__user">
+                      ${comment.user_name}
+                    </div>
+                    <div class= "comment__body__head__time">
+                      ${comment.created_at}
+                    </div>
+                    <div class= "comment__body__text">
+                      ${text}
+                    </div>
+                  </div>
+                </div>`
+    return html;
+  }
   $('#new_comment').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +28,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.comment').append(html);
+      $('.textbox').val('');
+      $('.form__submit').prop('disabled', false);
     })
   })
 })
