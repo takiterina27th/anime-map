447a861b62f520cd7d6286fa4f616e9aa2a28eb8
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-15 17:21:59 +0900

    コントローラーのリダイレクトするコードを削除
    詳細：
    createアクションdestroyアクションが動いた場合にリダイレクトする設定になっていたので、redirect_backを削除した。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = Like.create(user_id: current_user.id, post_id: @post.id)
-    redirect_back(fallback_location: post_path(@post.id))
+    # redirect_back(fallback_location: post_path(@post.id))
   end
 
   def destroy
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    redirect_back(fallback_location: post_path(params[:post_id]))
+    # redirect_back(fallback_location: post_path(params[:post_id]))
   end
 end
