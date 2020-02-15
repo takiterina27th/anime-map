a3e5609046d8a9eeb0a4e124806356aaf7633f3f
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-15 17:37:05 +0900

    コメントアウトを削除
    詳細：
    機能実装が一通り完了したので置いておいたコメントアウトを削除した。
    コメント機能のコメントアウトの削除を失念していたのでついでに削除した。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,11 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = Like.create(user_id: current_user.id, post_id: @post.id)
-    # redirect_back(fallback_location: post_path(@post.id))
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
-    # redirect_back(fallback_location: post_path(params[:post_id]))
   end
 end
