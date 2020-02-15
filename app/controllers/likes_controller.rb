606e55f818d8357516e38de743ce29a2ed459ac7
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-15 17:26:17 +0900

    削除時にpost_idを設定
    詳細：
    削除するいいねのpost_idを指定していないとundefined method `id' for nil:NilClassというエラーが出るのでdestroyアクションに@post = Post.find(params[:post_id])を記述。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,6 +6,7 @@ class LikesController < ApplicationController
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     @like.destroy
     # redirect_back(fallback_location: post_path(params[:post_id]))
