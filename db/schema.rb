51a98db004fdd26204106651fe39175dc85b6c39
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-22 22:27:50 +0900

    postテーブルにlatitudeカラム及びlongitudeカラムを追加
    詳細：
    postテーブルに、
    latitude: placeの緯度
    longitude: placeの経度
    の情報を追加できるようにカラムを作成した。
    データの桁数を揃えること無くそのまま保管したいときはfloat型を使用する。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_19_032331) do
+ActiveRecord::Schema.define(version: 2020_02_16_030512) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text", null: false
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2020_01_19_032331) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["name"], name: "index_posts_on_name"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
