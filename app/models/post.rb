0a01e547813496b18512bd7d9ee739e9e46189fc
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-22 22:50:59 +0900

    after_validation :geocode(フォームで場所を入力することで緯度と経度を取得する)
    詳細：
    after_validationによってバリデーションが行われた直後(テーブル内容に変更があった時)に geocode を実行できるようにした。
    geocode の内容はGoogleのgeocodingするAPIを叩いて、返ってきた緯度と経度の情報をpostテーブルの latitudeと　longitudeに挿入する。

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,15 @@ class Post < ApplicationRecord
   validates :description, presence: true
 
   mount_uploader :image, ImageUploader
+
+  after_validation :geocode
+
+  private
+  def geocode
+    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.place.gsub(" ", "")+"&key=#{ ENV['GOOGLE_MAP_API_KEY'] }")
+    res = HTTP.get(uri).to_s
+    response = JSON.parse(res)
+    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
+    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
+  end
 end
