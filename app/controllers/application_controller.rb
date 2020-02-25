b27b78fe94d87968ca93cc11d330df0b0306d0d4
takiterina27th <nozomeel5296@yahoo.co.jp>
2020-02-25 23:17:32 +0900

    Basic認証を導入
    詳細：
    Basic認証によるログインの要求は全てのコントローラで行う。Basic認証の処理をapplication_controller.rbにて、private以下にメソッドとして定義し、before_actionで呼び出すように実装した。
    また、usernameとpasswordを環境変数に設定した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_search
+  before_action :basic_auth
 
   def set_search
     @q = Post.ransack(params[:q])
@@ -19,5 +20,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     root_path
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
